6f0181b8bccfba85642ab6325d6294fcbaea03f1
Benjamin Cheng <bencheng200@gmail.com>
2020-11-12 05:14:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,15 @@ def sort_array_desc (array)
     -(a<=>b)
   end
 end
+
+def sort_array_char_count (array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
